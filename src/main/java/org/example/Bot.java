2a084f66b752b8a6a2a0fb04fe0e27fda6be8e8a
Mihsa-dev <misha.panovchik@gmail.com>
2024-12-13 18:42:26 +0500

    Функции, относящиеся к телеграмму пренесены в класс Bot; Добавлена обработка плохого конца игры; Добавлена обработка хорошего конца игры;

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -20,33 +20,15 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import static org.example.Constant.*;
 
 public class Bot extends TelegramLongPollingBot {
-    public final Queue<Update> receiveQueue = new ConcurrentLinkedQueue<>();
-    private boolean working = false;
     private HashMap<String, User> users = new HashMap<>();
-    private Field field;
 
     @Override
     public void onUpdateReceived(Update update) {
-        if(working){
-            receiveQueue.add(update);
-        }
-        else{
-            working = true;
-            executeUpdate(update);
-        }
-    }
-
-    private void executeUpdate(Update update){
         if (update.hasMessage() && update.getMessage().hasText()) {
             if (update.getMessage().getText().equals("/start")) {
                 String chatId = String.valueOf(update.getMessage().getChatId());
                 users.put(chatId, new User(chatId));
                 SendMessage msg = new SendMessage();
-//                    int messageId = update.getMessage().getMessageId();
-//                    msg.setText(ModeSelection);
-//                    msg.setChatId(update.getMessage().getChatId());
-//                    msg.setReplyMarkup(getInlineKeyboardButton(chatId, messageId));
-//                    execute(msg);
                 sendMessage(msg, ModeSelection, chatId, getInlineKeyboardButton(chatId, update.getMessage().getMessageId()));
             }
         } else if (update.hasCallbackQuery()) {
@@ -65,27 +47,20 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     public InlineKeyboardMarkup getInlineKeyboardButton(String chatId, int messageId) {
-        deleteLastMessage(chatId, messageId);
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
         InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
         InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
-        InlineKeyboardButton inlineKeyboardButton3 = new InlineKeyboardButton();
         inlineKeyboardButton1.setText(Training);
         inlineKeyboardButton1.setCallbackData("-hОбучение");
         inlineKeyboardButton2.setText(Standart);
         inlineKeyboardButton2.setCallbackData("-sСтандартный");
-        inlineKeyboardButton3.setText(RealTime);
-        inlineKeyboardButton3.setCallbackData("-rреальное_время");
         List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
         List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
-        List<InlineKeyboardButton> keyboardButtonsRow3 = new ArrayList<>();
         keyboardButtonsRow1.add(inlineKeyboardButton1);
         keyboardButtonsRow2.add(inlineKeyboardButton2);
-        keyboardButtonsRow3.add(inlineKeyboardButton3);
         List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
         rowList.add(keyboardButtonsRow1);
         rowList.add(keyboardButtonsRow2);
-        rowList.add(keyboardButtonsRow3);
         inlineKeyboardMarkup.setKeyboard(rowList);
         return inlineKeyboardMarkup;
     }
@@ -110,52 +85,34 @@ public class Bot extends TelegramLongPollingBot {
                 break;
             case "-s":
                 //по идее нам реализацию каждой из функций для обработки режимов нужно сделать в отдельном файле/классе с логикой бота
-                //field = new Field(12, 8);
                 users.get(chatId).setMyField(12, 8);
                 playStandart(chatId, messageId);
+
                 break;
-            case "-r":
-                message.setText("Сработал реальное_время");
-                message.setChatId(chatId);
-                try {
-                    execute(message);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-                break;
+
             //срабатывание кнопки из поля
             case "-b":
+
                 String comand = callData.substring(2);
                 if (comand.equals("Флаг")) {
                     users.get(chatId).getMyField().setMode(false);
-                    working = false;
-                    if (!receiveQueue.isEmpty()){
-                        onUpdateReceived(receiveQueue.poll());
-                    }
                 } else if (comand.equals("Копать")) {
                     users.get(chatId).getMyField().setMode(true);
-                    working = false;
-                    if (!receiveQueue.isEmpty()){
-                        onUpdateReceived(receiveQueue.poll());
-                    }
-                    //System.out.println(users.get(chatId).getMyField().getMode());
                 }
                 //пришла кнопка игрового поля
                 else {
-                    if (users.get(chatId).getMyField().getIsEndGame()) {
-                        SendMessage msg = new SendMessage();
-//                            msg.setText(ModeSelection);
-//                            msg.setChatId(chatId);
-//                            msg.setReplyMarkup(getInlineKeyboardButton(chatId, messageId));
-//                            execute(msg);
-                        sendMessage(msg, ModeSelection, chatId, getInlineKeyboardButton(chatId, messageId));
+                    users.get(chatId).getMyField().updateData(comand);
+                    System.out.println(users.get(chatId).getMyField().getCountClosedCells());
+                    System.out.println(users.get(chatId).getMyField().getCountBombs());
+                    if (users.get(chatId).getMyField().getCountClosedCells() == users.get(chatId).getMyField().getCountBombs()){
+                        deleteLastMessage(chatId, messageId);
+                        goodEndGame(users.get(chatId).getMyField(), chatId);
                         break;
                     }
-                    //System.out.println(callData.substring(2));
-                    users.get(chatId).getMyField().updateData(comand);
-                    if (users.get(chatId).getMyField().touchButtonByName(comand).isBomb()
-                            && !users.get(chatId).getMyField().touchButtonByName(comand).getFlag()) {
-                        users.get(chatId).getMyField().showBombs();
+                    if (users.get(chatId).getMyField().getIsEndGame()){
+                        deleteLastMessage(chatId, messageId);
+                        badEndGame(users.get(chatId).getMyField(), chatId);
+                        break;
                     }
                     playStandart(chatId, messageId);
                 }
@@ -185,19 +142,116 @@ public class Bot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-        working = false;
-        if (!receiveQueue.isEmpty()){
-            onUpdateReceived(receiveQueue.poll());
-        }
     }
 
     private void playStandart(String chatId, int messageId) {
         deleteLastMessage(chatId, messageId);
         SendMessage message = new SendMessage();
-//        message.setText(CountMines);
-//        message.setChatId(chatId);
-//        InlineKeyboardMarkup inlineKeyboardMarkup = field.getInlineKeyboardButton();
-//        message.setReplyMarkup(inlineKeyboardMarkup);
-        sendMessage(message, CountMines, chatId, users.get(chatId).getMyField().getInlineKeyboardButton());
+        sendMessage(message, CountMines+users.get(chatId).getMyField().getCountBombs(), chatId, getInlineKeyboardGame(users.get(chatId).getMyField()));
+    }
+
+    public InlineKeyboardMarkup getInlineKeyboardGame(Field field){
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+        for (int i = 0; i < field.getCountRow(); i++) {
+            List<InlineKeyboardButton> row = new ArrayList<>();
+            for (int j = 0; j < field.getCountCol(); j++) {
+                String buttonText = field.getButtons()[i][j].getEmoji();
+                String callbackData = String.format("-b%02d%02d", i, j);
+                // Создаем кнопку
+                InlineKeyboardButton button = InlineKeyboardButton.builder()
+                        .text(buttonText)
+                        .callbackData(callbackData)
+                        .build();
+                // Добавляем кнопку в строку
+                row.add(button);
+            }
+            // Добавляем строку в клавиатуру
+            rows.add(row);
+        }
+        List<InlineKeyboardButton> rowFlagDig = new ArrayList<>();
+        InlineKeyboardButton buttonFlag = new InlineKeyboardButton();
+        buttonFlag.setText(ModeFlag);
+        buttonFlag.setCallbackData("-bФлаг");
+        InlineKeyboardButton buttonDig = new InlineKeyboardButton();
+        buttonDig.setText(ModeDig);
+        buttonDig.setCallbackData("-bКопать");
+        rowFlagDig.add(buttonFlag);
+        rowFlagDig.add(buttonDig);
+        rows.add(rowFlagDig);
+        // Устанавливаем строки в клавиатуру
+        inlineKeyboardMarkup.setKeyboard(rows);
+        return inlineKeyboardMarkup;
+    }
+
+    private void badEndGame(Field field, String chatID){
+        SendMessage message = new SendMessage();
+        message.setChatId(chatID);
+        message.setText("Вы проиграли\nРасположение мин:");
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+        for (int i = 0; i < field.getCountRow(); i++) {
+            List<InlineKeyboardButton> row = new ArrayList<>();
+            for (int j = 0; j < field.getCountCol(); j++) {
+                if (field.getButtons()[i][j].isBomb()){
+                    field.getButtons()[i][j].setOpen(true);
+                }
+                String buttonText = field.getButtons()[i][j].getEmoji();
+                String callbackData = String.format("NoneData", i, j);
+                InlineKeyboardButton button = InlineKeyboardButton.builder()
+                        .text(buttonText)
+                        .callbackData(callbackData)
+                        .build();
+                row.add(button);
+            }
+            rows.add(row);
+        }
+
+        inlineKeyboardMarkup.setKeyboard(rows);
+
+        message.setReplyMarkup(inlineKeyboardMarkup);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void goodEndGame(Field field, String chatID){
+        SendMessage message = new SendMessage();
+        message.setChatId(chatID);
+        message.setText("Вы выиграли\nРасположение мин:");
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+        for (int i = 0; i < field.getCountRow(); i++) {
+            List<InlineKeyboardButton> row = new ArrayList<>();
+            for (int j = 0; j < field.getCountCol(); j++) {
+                if (field.getButtons()[i][j].getOpen()){
+                    field.getButtons()[i][j].setProperty(0);
+                }
+                else{
+                    field.getButtons()[i][j].setOpen(true);
+                    field.getButtons()[i][j].setFlag(false);
+                    field.getButtons()[i][j].setProperty(10);
+                }
+                String buttonText = field.getButtons()[i][j].getEmoji();
+                String callbackData = String.format("NoneData", i, j);
+                InlineKeyboardButton button = InlineKeyboardButton.builder()
+                        .text(buttonText)
+                        .callbackData(callbackData)
+                        .build();
+                row.add(button);
+            }
+            rows.add(row);
+        }
+
+        inlineKeyboardMarkup.setKeyboard(rows);
+
+        message.setReplyMarkup(inlineKeyboardMarkup);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/example/Field.java
+++ src/main/java/org/example/Field.java
@@ -13,6 +13,8 @@ public class Field {
     private Button[][] buttons;
     private int countRow;
     private int countCol;
+    private int countBombs;
+    private int countClosedCells;
     private boolean isEndGame;
     private boolean startedGame;
     //false - флаг, true - копать
@@ -23,7 +25,10 @@ public class Field {
         this.startedGame = false;
         this.mode = true;
         this.countRow = countRow;
-        this.countCol =countCol;
+        this.countCol = countCol;
+        this.countBombs = (int) Math.ceil(this.getCountRow() * this.getCountCol() * 16.0 / 100);
+        //16% поля - бомбы
+        this.countClosedCells = countRow * countCol;
         this.buttons = new Button[countRow][countCol];
         for (int i = 0; i < countRow; i++) {
             for (int j = 0; j < countCol; j++) {
@@ -62,6 +67,19 @@ public class Field {
     public void setCountCol(int countCol) {
         this.countCol = countCol;
     }
+    public int getCountClosedCells() {
+        return countClosedCells;
+    }
+    public void setCountClosedCells(int countClosedCells) {
+        this.countClosedCells = countClosedCells;
+    }
+    public int getCountBombs() {
+        return countBombs;
+    }
+    public void setCountBombs(int countBombs) {
+        this.countBombs = countBombs;
+    }
+
     public Button[][] getButtons(){return buttons;}
 
 
@@ -69,15 +87,18 @@ public class Field {
         Random random = new Random();
         int bomb_y_coord = random.nextInt(this.getCountRow());
         int bomb_x_coord = random.nextInt(this.getCountCol());
-        //System.out.println(bomb_y_coord);
 
         return new int[]{bomb_y_coord, bomb_x_coord};
     }
 
 
     private void openCells(int y_coord, int x_coord){
-        buttons[y_coord][x_coord].setOpen(true);
+        if (!buttons[y_coord][x_coord].getOpen() && !buttons[y_coord][x_coord].getFlag()){
+            buttons[y_coord][x_coord].setOpen(true);
+            countClosedCells -= 1;
+        }
         if (buttons[y_coord][x_coord].isBomb()){
+            showBombs();
             setIsEndGame(true);
             return;
         }
@@ -88,12 +109,20 @@ public class Field {
                 }
                 int curY = y_coord + i;
                 int curX = x_coord + j;
-                if (curY >= 0 && curY < 12 && curX >= 0 && curX < 8 && !buttons[curY][curX].getFlag() && !buttons[curY][curX].getOpen()){
+                if (curY >= 0 && curY < countRow && curX >= 0 && curX < countCol
+                        && !buttons[curY][curX].getFlag()
+                        && !buttons[curY][curX].getOpen()){
                     if(buttons[curY][curX].getProperty() == 0){
                         openCells(curY, curX);
                     }
                     else{
+                        if (buttons[curY][curX].isBomb()){
+                            showBombs();
+                            setIsEndGame(true);
+                            return;
+                        }
                         buttons[curY][curX].setOpen(true);
+                        countClosedCells -= 1;
                     }
                 }
             }
@@ -102,20 +131,17 @@ public class Field {
 
     private void startGame(int y_coord, int x_coord){
         this.setStartedGame(true);
-        int countBombs = (int) Math.ceil(this.getCountRow() * this.getCountCol() * 16.0 / 100); //16% поля - бомбы
-        //int countBombs = 16; //16% поля - бомбы
-
-        while (countBombs != 0) {
+        int bombs = countBombs;
+        while (bombs != 0) {
             int[] place = generateCoord();
-            //System.out.print(place[0]);
-            //System.out.println(" " + place[1]);
-            if((Math.abs(place[0] - y_coord) > 1 || Math.abs(place[1] - x_coord) > 1) && (!(buttons[place[0]][place[1]].isBomb()))){
+            if((Math.abs(place[0] - y_coord) > 1 || Math.abs(place[1] - x_coord) > 1)
+                    && (!(buttons[place[0]][place[1]].isBomb()))){
                 buttons[place[0]][place[1]].setProperty(9);
-                countBombs -= 1;
+                bombs -= 1;
             }
         }
-        for (int i = 0; i < 12; i++){
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < countRow; i++){
+            for (int j = 0; j < countCol; j++) {
                 if (buttons[i][j].isBomb()){
                     for (int y = -1; y <= 1; y++){
                         for (int x = -1; x <= 1; x++){
@@ -124,7 +150,8 @@ public class Field {
                             }
                             int curY = i + y;
                             int curX = j + x;
-                            if (curY >= 0 && curY < 12 && curX >= 0 && curX < 8 && !buttons[curY][curX].isBomb()){
+                            if (curY >= 0 && curY < countRow && curX >= 0 && curX < countCol
+                                    && !buttons[curY][curX].isBomb()){
                                 buttons[curY][curX].setProperty(buttons[curY][curX].getProperty()+1);
                             }
                         }
@@ -154,7 +181,8 @@ public class Field {
                             }
                             int curY = y_coord + i;
                             int curX = x_coord + j;
-                            if (curY >= 0 && curY < 12 && curX >= 0 && curX < 8 && buttons[curY][curX].getFlag()){
+                            if (curY >= 0 && curY < countRow && curX >= 0 && curX < countCol
+                                    && buttons[curY][curX].getFlag()){
                                 counterFlags += 1;
                             }
                         }
@@ -166,6 +194,7 @@ public class Field {
                 else{
                     buttons[y_coord][x_coord].setFlag(false);
                     if (buttons[y_coord][x_coord].isBomb()){
+                        showBombs();
                         setIsEndGame(true);
                         return;
                     }
@@ -174,60 +203,24 @@ public class Field {
                     }
                     else{
                         buttons[y_coord][x_coord].setOpen(true);
+                        countClosedCells -= 1;
                     }
                 }
             }
             else{
                 //флажок можно установить/снять только на закрытую
                 if (!buttons[y_coord][x_coord].getOpen()){
-
                     buttons[y_coord][x_coord].setFlag(!buttons[y_coord][x_coord].getFlag());
-
                 }
             }
         }
     }
 
-    public InlineKeyboardMarkup getInlineKeyboardButton(){
-        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
-        int rowsCount = this.getCountRow(); // 12 строк
-        int colsCount = this.getCountCol();  // 8 столбцов
-        for (int i = 0; i < rowsCount; i++) {
-            List<InlineKeyboardButton> row = new ArrayList<>();
-            for (int j = 0; j < colsCount; j++) {
-                String buttonText = buttons[i][j].getEmoji();
-                String callbackData = String.format("-b%02d%02d", i, j);
-                // Создаем кнопку
-                InlineKeyboardButton button = InlineKeyboardButton.builder()
-                        .text(buttonText)
-                        .callbackData(callbackData)
-                        .build();
-                // Добавляем кнопку в строку
-                row.add(button);
-            }
-            // Добавляем строку в клавиатуру
-            rows.add(row);
-        }
-        List<InlineKeyboardButton> rowFlagDig = new ArrayList<>();
-        InlineKeyboardButton buttonFlag = new InlineKeyboardButton();
-        buttonFlag.setText(ModeFlag);
-        buttonFlag.setCallbackData("-bФлаг");
-        InlineKeyboardButton buttonDig = new InlineKeyboardButton();
-        buttonDig.setText(ModeDig);
-        buttonDig.setCallbackData("-bКопать");
-        rowFlagDig.add(buttonFlag);
-        rowFlagDig.add(buttonDig);
-        rows.add(rowFlagDig);
-        // Устанавливаем строки в клавиатуру
-        inlineKeyboardMarkup.setKeyboard(rows);
-        return inlineKeyboardMarkup;
-    }
-
     public void showBombs(){
-        for (int i = 0; i < 12; i++){
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < countRow; i++){
+            for (int j = 0; j < countCol; j++) {
                 if (buttons[i][j].isBomb()){
+                    buttons[i][j].setFlag(false);
                     buttons[i][j].setOpen(true);
                 }
             }
